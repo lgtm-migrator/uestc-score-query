d97d7503a4dab43d382a20fc0dce5d3723b5095f
suntao <mrls@live.cn>
2017-01-28 12:32:13 +0800

    will use bs4 to parse html, and more oop

--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
-import uestc_query,os;
+import uestc_query
+import os
+
+username = os.getenv("TUSERNAME")
+password = os.getenv("TPASSWORD")
 
 def test_uestc_score_query():
-  username = os.getenv("TUSERNAME");
-  password = os.getenv("TPASSWORD");
-  res = uestc_query.query(username,password);
-  assert res != None
-  assert len(res) > 0
+    res = uestc_query.query(username, password)
+    assert res != None
+    assert len(res) > 0
+
+
+def test_uestc_score_login():
+    s = uestc_query.Session()
+    assert s.login(username, password) == True
--- uestc_query.py
+++ uestc_query.py
@@ -2,25 +2,72 @@
 import requests
 from pprint import pprint as pr
 from pyquery import PyQuery as pq
+from bs4 import BeautifulSoup
 
-def query(username,password):
+
+class Session(object):
+
+    def __init__(self):
+        self.conn = requests.session()
+
+    def login(self, username, password):
+        session = self.conn
+        r = session.get(
+            "https://idas.uestc.edu.cn/authserver/login")
+        d = pq(r.text)
+        # 从网页中获取动态生成的字段
+        lt = d('#casLoginForm > input[type="hidden"]:nth-child(5)').attr.value
+        execution = d(
+            '#casLoginForm > input[type="hidden"]:nth-child(7)').attr.value
+        data = {
+            "username": username,
+            "password": password,
+            "lt": lt,
+            "execution": execution,
+            "rmShown": 1,
+            "dllt": "userNamePasswordLogin",
+            "_eventId": "submit"
+        }
+        r = session.post(
+            "https://idas.uestc.edu.cn/authserver/login?service=http://portal.uestc.edu.cn/index.portal", data=data)
+        return self.is_login()
+
+    def is_login(self, username: str=None):
+        r = self.conn.get("https://idas.uestc.edu.cn/authserver/index.do")
+        soup = BeautifulSoup(r.text, 'lxml')
+        try:
+            login_id = soup.select(
+                "#auth_siderbar > div.auth_username > span > span")[0].text.strip()
+            if username != None and username == login_id:
+                return True
+            elif login_id:
+                return True
+        except Exception as err:
+            raise err
+            return False
+
+def query(username, password):
     session = requests.session()
-    r = session.get("http://idas.uestc.edu.cn/authserver/login?service=http%3A%2F%2Fportal.uestc.edu.cn%2F")
-    d = pq(r.text);
+    r = session.get(
+        "http://idas.uestc.edu.cn/authserver/login?service=http%3A%2F%2Fportal.uestc.edu.cn%2F")
+    d = pq(r.text)
     # 从网页中获取动态生成的字段
     lt = d('#casLoginForm > input[type="hidden"]:nth-child(5)').attr.value
-    execution = d('#casLoginForm > input[type="hidden"]:nth-child(7)').attr.value
+    execution = d(
+        '#casLoginForm > input[type="hidden"]:nth-child(7)').attr.value
     data = {
-        "username":username,
-        "password":password,
-        "lt":lt,
-        "execution":execution,
-        "rmShown":1,
-        "dllt":"userNamePasswordLogin",
-        "_eventId":"submit" 
+        "username": username,
+        "password": password,
+        "lt": lt,
+        "execution": execution,
+        "rmShown": 1,
+        "dllt": "userNamePasswordLogin",
+        "_eventId": "submit"
     }
-    r = session.post("http://idas.uestc.edu.cn/authserver/login?service=http://portal.uestc.edu.cn/index.portal",data=data);
-    r = session.get("http://eams.uestc.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action?projectType=MAJOR");
+    r = session.post(
+        "http://idas.uestc.edu.cn/authserver/login?service=http://portal.uestc.edu.cn/index.portal", data=data)
+    r = session.get(
+        "http://eams.uestc.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action?projectType=MAJOR")
     table = []
     d = pq(r.text)
     h = d(".gridtable:last thead tr th")
@@ -28,8 +75,7 @@ def query(username,password):
 
     for tr in c:
         line = {}
-        for td,i in zip(tr.getchildren(),h):
+        for td, i in zip(tr.getchildren(), h):
             line[i.text] = td.text and td.text.strip()
         table.append(line)
     return table
-
